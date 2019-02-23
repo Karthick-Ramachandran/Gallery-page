bf22d1506406e39005e3dfcbbe6c640b426b999e
Karthick <speak@karthick.tech>
2019-02-23 20:43:44 +0530

    landing page

---
+++ precache-manifest.f8d85aa29c6287d9b4efc73cbdd19efd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "14e4d6cc4e7ff1f74ebdfcd7e675cc26",
+    "url": "/static/media/image1.14e4d6cc.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "625cfe800cef600926b5",
+    "url": "/static/js/main.625cfe80.chunk.js"
+  },
+  {
+    "revision": "0f5c324eba0293e31196",
+    "url": "/static/js/2.0f5c324e.chunk.js"
+  },
+  {
+    "revision": "625cfe800cef600926b5",
+    "url": "/static/css/main.8df0b0d9.chunk.css"
+  },
+  {
+    "revision": "ab0ecbcf208219b2fcaeacda202ccf6d",
+    "url": "/index.html"
+  }
+];
